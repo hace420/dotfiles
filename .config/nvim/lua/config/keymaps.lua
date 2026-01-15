a54766f9bcc855df476c7a36acb7dd7e7b900fa5
hace420 <chrisboomboom4@gmail.com>
2026-01-14 20:39:52 -0500

    updated nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+
+vim.keymap.set("n", "<leader>jr", function()
+  local file = vim.fn.expand("%:t:r")
+  vim.cmd("w")
+  vim.cmd("botright vsplit | terminal javac % && java " .. file)
+end, { desc = "Java compile & run (right terminal)" })
+
+
+vim.keymap.set({ "n", "t" }, "<leader>jf", function()
+  if vim.bo.buftype == "terminal" then
+    vim.cmd("bd!")
+  end
+end, { desc = "Close terminal buffer" })
