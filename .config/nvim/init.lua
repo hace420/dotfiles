5e2daf08b1f8d5c1fa0d27aeeaa3574089e9b027
hace420 <chrisboomboom4@gmail.com>
2026-01-14 23:00:53 -0500

    more stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.lsp.enable({ "lua_ls" })
+--vim.lsp.enable({ "lua_ls" })
+
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "lua_ls", "jdtls" }
+})
